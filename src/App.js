68a4e5f7ddb2527ec367c62de760f6f4a6f8ea4c
Milky Raven <heartonllamas@gmail.com>
2022-11-20 22:01:32 +0100

    working on list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Todo from './components/Todo';
+import React, {useState} from 'react';
+import TodoList from './components/TodoList';
 
+let todoArray =[{id:1, title: 'Do laundry', completed: false}];
 function App() {
+  const [todos, setTodos] = useState(todoArray)
+  
   return (
     <div>
-    <Todo />
+    <TodoList todoList={todos}/>
     <input type="text" />
     <button> Add </button>
     <button> Clear all </button>
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList(props) {
+  console.log(props.todoList[0].title)
+  return (
+    <div>
+    
+    </div>
+  )
+}
